12f6645425fad93c08a5a878238cf94638a13ac9
lessane-land <lessane.vanesa@gmail.com>
2026-07-20 12:56:51 +0200

    v9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "camino-v17";
+const CACHE = "camino-v20";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./config.js", "./config.js", "./icon-192.png", "./icon-512.png", "./icon-180.png"];
 
 self.addEventListener("install", e => {
